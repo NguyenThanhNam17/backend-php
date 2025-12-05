dde6726a6db01e981a18c0c4ecf9fad3144e3221
NguyenThanhNam17 <122538702+NguyenThanhNam17@users.noreply.github.com>
2025-12-06 01:22:18 +0700

    test

--- public/index.php
+++ public/index.php
@@ -6,4 +6,6 @@ $result = $db->query("SELECT datetime('now') as current_time")->fetchArray();
 
 echo "<h1>PHP + SQLite Deploy bằng Render + GitHub Actions</h1>";
 echo "<p>Thời gian hiện tại trong DB: " . $result['current_time'] . "</p>";
+echo "<p>Thi cuối kì mã nguồn mở</p>";
+echo "<p>Nguyễn Thành Nam - DH52201080</p>";
 ?>
