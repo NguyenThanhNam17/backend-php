25ed07046fa65ec2f99eae545a2a3e1440c63771
NguyenThanhNam17 <122538702+NguyenThanhNam17@users.noreply.github.com>
2025-12-14 17:19:59 +0700

    updatetime

--- public/index.php
+++ public/index.php
@@ -2,11 +2,10 @@
 require_once __DIR__ . '/../app/database.php';
 
 $db = getDB();
-$result = $db->query("SELECT datetime('now') as current_time")->fetchArray();
+$result = $db->query("SELECT datetime('now'+'+7 hours') as current_time")->fetchArray();
 
 echo "<h1>PHP + SQLite Deploy bằng Render + GitHub Actions</h1>";
 echo "<p>Thời gian hiện tại trong DB: " . $result['current_time'] . "</p>";
 echo "<p>Thi cuối kì mã nguồn mở</p>";
 echo "<p>Nguyễn Thành Nam - DH52201080</p>";
-echo "<p>hello</p>"
 ?>
